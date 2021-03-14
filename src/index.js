a4422c44a60481706642bf56e5a1f7e6db3dcf88
winston-lim <winston_lim1@hotmail.com>
2021-03-14 11:15:47 +0800

    Add socketio and refactor index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 const express = require('express');
+const http = require('http');
 const path = require('path');
+const socketio = require('socket.io');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 const port = process.env.PORT || 3000;
 
 const publicDirectoryPath = path.join(__dirname, '../public/');
 
 app.use(express.static(publicDirectoryPath));
 
-app.listen(port, ()=> {
+server.listen(port, ()=> {
   console.log(`Server is up on port ${port}`);
 })
